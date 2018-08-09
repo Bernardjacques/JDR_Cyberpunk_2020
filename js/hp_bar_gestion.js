f20863e642a366903a0733df6b6ab14c62116953
Félix Namêche <35733439+FelixNameche@users.noreply.github.com>
2018-08-09 11:53:11 +0200

    Update hp_bar_gestion.js
    Indentation revue, gain de 88 lignes

--- js/hp_bar_gestion.js
+++ js/hp_bar_gestion.js
@@ -1,422 +1,334 @@
 // ============================ HP Bar Gestion ============================
-
-let var_color = 0
-
+var var_color = 0;
 // ============= Descrease HP ==================
-function life_hit() 
-{
-    if(var_color == 0)
-    {
+function life_hit() {
+    if (var_color == 0) {
         document.getElementById("life1").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 1)
-    {
+    else if (var_color == 1) {
         document.getElementById("life2").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 2)
-    {
+    else if (var_color == 2) {
         document.getElementById("life3").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 3)
-    {
+    else if (var_color == 3) {
         document.getElementById("life4").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 4)
-    {
+    else if (var_color == 4) {
         document.getElementById("life5").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 5)
-    {
+    else if (var_color == 5) {
         document.getElementById("life6").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 6)
-    {
+    else if (var_color == 6) {
         document.getElementById("life7").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 7)
-    {
+    else if (var_color == 7) {
         document.getElementById("life8").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 8)
-    {
+    else if (var_color == 8) {
         document.getElementById("life9").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 9)
-    {
+    else if (var_color == 9) {
         document.getElementById("life10").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 10)
-    {
+    else if (var_color == 10) {
         document.getElementById("life11").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 11)
-    {
+    else if (var_color == 11) {
         document.getElementById("life12").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 12)
-    {
+    else if (var_color == 12) {
         document.getElementById("life13").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 13)
-    {
+    else if (var_color == 13) {
         document.getElementById("life14").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 14)
-    {
+    else if (var_color == 14) {
         document.getElementById("life15").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 15)
-    {
+    else if (var_color == 15) {
         document.getElementById("life16").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 16)
-    {
+    else if (var_color == 16) {
         document.getElementById("life17").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 17)
-    {
+    else if (var_color == 17) {
         document.getElementById("life18").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 18)
-    {
+    else if (var_color == 18) {
         document.getElementById("life19").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 19)
-    {
+    else if (var_color == 19) {
         document.getElementById("life20").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 20)
-    {
+    else if (var_color == 20) {
         document.getElementById("life21").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 21)
-    {
+    else if (var_color == 21) {
         document.getElementById("life22").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 22)
-    {
+    else if (var_color == 22) {
         document.getElementById("life23").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 23)
-    {
+    else if (var_color == 23) {
         document.getElementById("life24").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 24)
-    {
+    else if (var_color == 24) {
         document.getElementById("life25").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 25)
-    {
+    else if (var_color == 25) {
         document.getElementById("life26").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 26)
-    {
+    else if (var_color == 26) {
         document.getElementById("life27").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 27)
-    {
+    else if (var_color == 27) {
         document.getElementById("life28").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 28)
-    {
+    else if (var_color == 28) {
         document.getElementById("life29").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 29)
-    {
+    else if (var_color == 29) {
         document.getElementById("life30").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 30)
-    {
+    else if (var_color == 30) {
         document.getElementById("life31").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 31)
-    {
+    else if (var_color == 31) {
         document.getElementById("life32").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 32)
-    {
+    else if (var_color == 32) {
         document.getElementById("life33").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 33)
-    {
+    else if (var_color == 33) {
         document.getElementById("life34").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 34)
-    {
+    else if (var_color == 34) {
         document.getElementById("life35").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 35)
-    {
+    else if (var_color == 35) {
         document.getElementById("life36").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 36)
-    {
+    else if (var_color == 36) {
         document.getElementById("life37").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 37)
-    {
+    else if (var_color == 37) {
         document.getElementById("life38").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 38)
-    {
+    else if (var_color == 38) {
         document.getElementById("life39").style.backgroundColor = "red";
         var_color += 1;
     }
-    else if(var_color == 39)
-    {
+    else if (var_color == 39) {
         document.getElementById("life40").style.backgroundColor = "red";
         var_color += 1;
     }
-    else
-    {
-        console.log("Fail")
+    else {
+        console.log("Fail");
     }
-}  
-
+}
 // ============= Increase HP ===================
-
-function life_hit2() 
-{
-    if(var_color == 1)
-    {
+function life_hit2() {
+    if (var_color == 1) {
         document.getElementById("life1").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 2)
-    {
+    else if (var_color == 2) {
         document.getElementById("life2").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 3)
-    {
+    else if (var_color == 3) {
         document.getElementById("life3").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 4)
-    {
+    else if (var_color == 4) {
         document.getElementById("life4").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 5)
-    {
+    else if (var_color == 5) {
         document.getElementById("life5").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 6)
-    {
+    else if (var_color == 6) {
         document.getElementById("life6").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 7)
-    {
+    else if (var_color == 7) {
         document.getElementById("life7").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 8)
-    {
+    else if (var_color == 8) {
         document.getElementById("life8").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 9)
-    {
+    else if (var_color == 9) {
         document.getElementById("life9").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 10)
-    {
+    else if (var_color == 10) {
         document.getElementById("life10").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 11)
-    {
+    else if (var_color == 11) {
         document.getElementById("life11").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 12)
-    {
+    else if (var_color == 12) {
         document.getElementById("life12").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 13)
-    {
+    else if (var_color == 13) {
         document.getElementById("life13").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 14)
-    {
+    else if (var_color == 14) {
         document.getElementById("life14").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 15)
-    {
+    else if (var_color == 15) {
         document.getElementById("life15").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 16)
-    {
+    else if (var_color == 16) {
         document.getElementById("life16").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 17)
-    {
+    else if (var_color == 17) {
         document.getElementById("life17").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 18)
-    {
+    else if (var_color == 18) {
         document.getElementById("life18").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 19)
-    {
+    else if (var_color == 19) {
         document.getElementById("life19").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 20)
-    {
+    else if (var_color == 20) {
         document.getElementById("life20").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 21)
-    {
+    else if (var_color == 21) {
         document.getElementById("life21").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 22)
-    {
+    else if (var_color == 22) {
         document.getElementById("life22").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 23)
-    {
+    else if (var_color == 23) {
         document.getElementById("life23").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 24)
-    {
+    else if (var_color == 24) {
         document.getElementById("life24").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 25)
-    {
+    else if (var_color == 25) {
         document.getElementById("life25").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 26)
-    {
+    else if (var_color == 26) {
         document.getElementById("life26").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 27)
-    {
+    else if (var_color == 27) {
         document.getElementById("life27").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 28)
-    {
+    else if (var_color == 28) {
         document.getElementById("life28").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 29)
-    {
+    else if (var_color == 29) {
         document.getElementById("life29").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 30)
-    {
+    else if (var_color == 30) {
         document.getElementById("life30").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 31)
-    {
+    else if (var_color == 31) {
         document.getElementById("life31").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 32)
-    {
+    else if (var_color == 32) {
         document.getElementById("life32").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 33)
-    {
+    else if (var_color == 33) {
         document.getElementById("life33").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 34)
-    {
+    else if (var_color == 34) {
         document.getElementById("life34").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 35)
-    {
+    else if (var_color == 35) {
         document.getElementById("life35").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 36)
-    {
+    else if (var_color == 36) {
         document.getElementById("life36").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 37)
-    {
+    else if (var_color == 37) {
         document.getElementById("life37").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 38)
-    {
+    else if (var_color == 38) {
         document.getElementById("life38").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 39)
-    {
+    else if (var_color == 39) {
         document.getElementById("life39").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else if(var_color == 40)
-    {
+    else if (var_color == 40) {
         document.getElementById("life40").style.backgroundColor = "green";
         var_color -= 1;
     }
-    else
-    {
-        console.log("Fail")
+    else {
+        console.log("Fail");
     }
-} 
+}
